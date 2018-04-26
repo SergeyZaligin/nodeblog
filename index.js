4dacf3b080159a2aafa262b9f5b4dbfd08359c6b
sergey <sergey>
2018-04-26 13:59:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+let arr = ['item1', 'item2', 'item3'];
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    arr: arr
+  });
+});
+
+app.get('/create', function(req, res) {
+  res.render('create');
+});
+
+app.post('/create', function(req, res) {
+  arr.push(req.body.text);
+
+  res.redirect('/');
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
